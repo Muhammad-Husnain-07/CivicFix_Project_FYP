cf513cb19dede7afc7ad5ad15c6b9b2c962bf45f
unknown <husnain.khan@timestreamgroup.com>
2025-02-02 18:54:38 +0500

    Environment Vairables Issues Fixed

--- CivicFix_Backend/civicfixDjango/civicfixDjango/settings.py
+++ CivicFix_Backend/civicfixDjango/civicfixDjango/settings.py
@@ -12,13 +12,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
-env =  environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env =  environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
